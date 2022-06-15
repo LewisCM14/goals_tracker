ffffeb672c3856f60a7f613f3b5131cad2315b4f
LewisCM14 <Lewiscmartin@live.co.uk>
2022-06-15 22:58:52 +0100

    handle blank form input submission

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,41 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  // Handles dynamic form display on submit
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length>0) {
+        setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    //  upon submit trims any whitespace and checks the string length.
+    // if 0, calls the setIsValid function, updating its state to false
+    // before executing return and preventing the formSubmitHandler from completing.
+    if (enteredValue.trim().length === 0) {
+        setIsValid(false);
+        return;
+    }
     props.onAddGoal(enteredValue);
   };
 
+  /* if 'isValid' is false, set the color property to red, else set to black */
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
